0d1e7d3b92165574dab56aa11c449e2fb29d1ae6
Emma1005 <nliu01@email.wm.edu>
2017-05-26 17:26:51 -0400

    add lda.py
    share code for LDA

---
+++ lda.py
@@ -0,0 +1,52 @@
+from nltk.tokenize import RegexpTokenizer
+from stop_words import get_stop_words
+from nltk.stem.porter import PorterStemmer
+from gensim import corpora, models
+import gensim
+import codecs
+
+tokenizer = RegexpTokenizer(r'\w+')
+
+# create English stop words list
+en_stop = get_stop_words('en')
+
+# Create p_stemmer of class PorterStemmer
+p_stemmer = PorterStemmer()
+
+# list for tokenized documents in loop
+texts = []
+
+# loop through document list
+with codecs.open("Presidential_debate_Hofstra_uni", "r", encoding="utf-8",errors="ignore") as file:
+	for line in file:
+
+	# clean and tokenize document string
+		raw = line.lower()
+		tokens = tokenizer.tokenize(raw)
+
+	# remove stop words from tokens
+		stopped_tokens = [i for i in tokens if not i in en_stop]
+
+	# stem tokens
+		stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+
+	# add tokens to list
+		texts.append(stemmed_tokens)
+
+# turn out tokenized documents into a id <-> term dictionary
+dictionary = corpora.Dictionary(texts)	
+
+# convert tokenized documents into a docuemnt-term matrix
+corpus = [dictionary.doc2bow(text) for text in texts]
+
+# generate LDA model
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = 30, id2word = dictionary, passes=20)
+
+print(ldamodel.print_topics(num_topics = 30, num_words = 20))
+
+
+
+
+
+
+
